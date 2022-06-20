1e93c9141121e3bb8c89ac63e5d366f1aa1c4f2d
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-06-20 23:48:32 +0600

    egg timer

--- egg_timer/ViewController.swift
+++ egg_timer/ViewController.swift
@@ -9,11 +9,111 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var SliderButton: UISlider!
+    @IBOutlet weak var softButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    @IBOutlet weak var mediumButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    var soft:Int = 5
+    var medium: Int = 10
+    var hard: Int = 15
+    var counterSoft:Int = 0
+    var counterMed: Int = 0
+    var counterHard: Int = 0
+    var timer = Timer ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameLabel.text = "Please select egg type"
+        timeLabel.text = ""
     }
-
-
+    
+    @IBAction func softEgg(_ sender: UIButton) {
+        reset()
+        softButton.isEnabled = false
+        mediumButton.isEnabled = true
+        hardButton.isEnabled = true
+        nameLabel.text = "Soft"
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(stepBackSoft), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func mediumEgg(_ sender: UIButton) {
+    reset()
+        softButton.isEnabled = true
+        mediumButton.isEnabled = false
+        hardButton.isEnabled = true
+        nameLabel.text = "Medium"
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(stepBackMedium), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func hardEgg(_ sender: UIButton) {
+        reset()
+        softButton.isEnabled = true
+        mediumButton.isEnabled = true
+        hardButton.isEnabled = false
+        nameLabel.text = "Hard"
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(stepBackHard), userInfo: nil, repeats: true)
+    }
+    
+    @objc func stepBackSoft (){
+        if counterSoft != soft{
+            counterSoft += 1
+                timeLabel.text = "\(counterSoft)"
+            SliderButton.value = Float (counterSoft)
+            print("Soft: \(counterSoft)")
+        }else {
+            timer.invalidate()
+            timeLabel.text = "\(soft)"
+            nameLabel.text = "Your egg is ready"
+            timer.invalidate()
+        }
+        }
+    
+    @objc func stepBackMedium (){
+        if counterMed != medium{
+            counterMed += 1
+                timeLabel.text = "\(counterMed)"
+            SliderButton.value = Float (counterMed)
+            print("Med: \(counterMed)")
+        }else {
+            timer.invalidate()
+            timeLabel.text = "\(medium)"
+            nameLabel.text = "Your egg is ready"
+            timer.invalidate()
+        }
+        }
+    
+    @objc func stepBackHard (){
+        if counterHard != hard{
+            counterHard += 1
+                timeLabel.text = "\(counterHard)"
+            SliderButton.value = Float (counterHard)
+            print("Hard: \(counterHard)")
+        }else {
+            timer.invalidate()
+            nameLabel.text = "Your egg is ready"
+            timeLabel.text = "\(hard)"
+            timer.invalidate()
+        }
+        }
+    
+    func reset() {
+        softButton.isEnabled = true
+        mediumButton.isEnabled = true
+        hardButton.isEnabled = true
+        nameLabel.text = "Stop"
+        SliderButton.value = 0
+        counterSoft = 0
+        counterMed = 0
+        counterHard = 0
+        timeLabel.text = "0"
+    }
 }
+    
+    
+
 
