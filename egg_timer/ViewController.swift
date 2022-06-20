173c7f03664166df72b708843bdfbeb54a996bc6
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-06-20 20:24:31 +0600

    Initial Commit

---
+++ egg_timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  egg_timer
+//
+//  Created by Faiaz Ibraev on 20/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
